57d87c00f366f7c81073f83bb2c388a22b75352a
lupita alvarez <alv.lu1305@gmail.com>
2021-09-10 13:03:11 -0700

    tests pass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import styled from 'styled-components';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +14,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+const [starWarsData, setStarWarsData] = useState([])
+const [error, setError] = useState(null);
+
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setStarWarsData(res.data)
+    })
+    .catch(err => {
+      setError(`Please try again later`) 
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {starWarsData.map(item => {
+        return <Character data={item} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    background-color: lightblue;
+    color: black;
+    opacity:0.6
+`
+
+const StyledName = styled.h2`
+    color: teal;
+    border-style: outset;
+`
+
+const Character = (props) => {
+const {data}=props
+    return (
+        <StyledCharacter>
+            <div className='name'>
+                <StyledName>{data.name}</StyledName>
+            </div>
+        <div className='info'>
+            <p>Height: {data.height}</p>
+            <p>Mass: {data.mass}</p>
+            <p>Hair color: {data.hair_color}</p>
+            <p>Skin Color: {data.skin_color}</p>
+            <p>Birth Year: {data.birth_year}</p>
+            <p>Eye Color: {data.eye_color}</p>
+            <p>Birth Year: {data.birth_year}</p>
+            <p>Gender: {data.gender}</p>
+        </div>
+        </StyledCharacter>
+    )
+ }
+
+ export default Character;
